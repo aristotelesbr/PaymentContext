7252b9ea2889adb5451d7bd5f5211a687f961c5b
Aristoteles <aristotelesbr2014@gmail.com>
2020-08-02 02:25:25 -0300

    add domain

---
+++ PaymentContext.Test/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+  [TestClass]
+  public class StudentTests
+  {
+    [TestMethod]
+    public void AddSubscription()
+    {
+      var subscription = new Subscription(null);
+      var studend = new Student("Aristóteles", "Coutinho", "123123", "ari@mail.com");
+
+      studend.AddSubscription(subscription);
+    }
+  }
+}
